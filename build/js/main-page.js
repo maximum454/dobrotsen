6a99ee7e55e41419651e946dbb8c7f40af7d081a
Maxim Karachyov <karachyov.m@gmail.com>
2022-04-21 22:27:20 +0300

    nav, slider, pixel

--- build/js/main-page.js
+++ build/js/main-page.js
@@ -1,16 +1,28 @@
+const swiperNav = new Swiper('.swiper-nav', {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+})
+
+const swiperMain = new Swiper('.swiper-main', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+})
+
 const swiperBrand = new Swiper('.swiper-brand', {
-    // Default parameters
     slidesPerView: 1.5,
     spaceBetween: 16,
 })
 
 const swiperRecipes = new Swiper('.swiper-recipes', {
-    // Default parameters
     slidesPerView: 1.5,
     spaceBetween: 16,
 })
 
 const swiperCatalog = new Swiper('.swiper-catalog', {
-    slidesPerView: 2.3,
+    slidesPerView: 2.2,
     spaceBetween: 16,
 })
